484a959860c169c8ffe40784cbbec8685910caeb
jinholee17 <jinho_lee@brown.edu>
2024-02-12 16:26:50 -0500

    fix: handlers tests for parsing edits

--- src/test/java/edu/brown/cs/student/Tests/CSVHandlersTest.java
+++ src/test/java/edu/brown/cs/student/Tests/CSVHandlersTest.java
@@ -281,7 +281,8 @@ public class CSVHandlersTest {
         moshi
             .adapter(DataSuccessResponse.class)
             .fromJson(new Buffer().readFrom(clientConnection2.getInputStream()));
-    String expected = "DataSuccessResponse[response_type=success,responseMap=[[Cranston,\"77,145.00\",\"95,763.00\",\"38,269.00\"]]]";
+    String expected = "DataSuccessResponse[response_type=success,responseMap={column=,value=CRANSTON"
+        + ",results=[[Cranston,\"77,145.00\",\"95,763.00\",\"38,269.00\"]]}]";
 
     assertEquals(response.toString().trim().replaceAll("\\s", ""),
         expected.trim().replaceAll("\\s", ""));
@@ -307,7 +308,8 @@ public class CSVHandlersTest {
         moshi
             .adapter(DataSuccessResponse.class)
             .fromJson(new Buffer().readFrom(clientConnection2.getInputStream()));
-    String expected = "DataSuccessResponse[response_type=success,responseMap=[]]";
+    String expected = "DataSuccessResponse[response_type=success,responseMap={column=lol,value=hi,"
+        + "results=[]}]";
 
     assertEquals(response.toString().trim().replaceAll("\\s", ""),
         expected.trim().replaceAll("\\s", ""));
@@ -334,8 +336,8 @@ public class CSVHandlersTest {
             .adapter(DataSuccessResponse.class)
             .fromJson(new Buffer().readFrom(clientConnection2.getInputStream()));
     String expected = "DataSuccessResponse[response_type=success,responseMap="
-        + "[[RI,Asian-PacificIslander,\"$1,080.09\",18956.71657,$1.02,4%,Multiracial],"
-        + "[RI,Multiracial,$971.89,8883.049171,$0.92,2%,Hispanic/Latino]]]";
+        + "{value=Multiracial,results=[[RI,Asian-PacificIslander,\"$1,080.09\",18956.71657,$1.02,4%,Multiracial],"
+        + "[RI,Multiracial,$971.89,8883.049171,$0.92,2%,Hispanic/Latino]]}]";
 
     assertEquals(response.toString().trim().replaceAll("\\s", ""),
         expected.trim().replaceAll("\\s", ""));
