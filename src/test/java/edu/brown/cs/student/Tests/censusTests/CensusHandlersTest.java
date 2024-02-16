593411b45346755b3edbe282bc402e75bb509043
jinholee17 <jinho_lee@brown.edu>
2024-02-16 00:41:34 -0500

    tests: more tests for census handlers

--- src/test/java/edu/brown/cs/student/Tests/censusTests/CensusHandlersTest.java
+++ src/test/java/edu/brown/cs/student/Tests/censusTests/CensusHandlersTest.java
@@ -1,5 +1,10 @@
 package edu.brown.cs.student.Tests.censusTests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.squareup.moshi.Moshi;
+import edu.brown.cs.student.main.server.DataSuccessResponse;
+import edu.brown.cs.student.main.server.ErrorResponse;
 import edu.brown.cs.student.main.server.censusHandler.CensusHandler;
 import edu.brown.cs.student.main.server.censusHandler.CensusResult;
 import java.io.IOException;
@@ -8,9 +13,11 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import okio.Buffer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import spark.Spark;
 
 public class CensusHandlersTest {
@@ -61,6 +68,136 @@ public class CensusHandlersTest {
 
     clientConnection.connect();
     return clientConnection;
+  }
+
+  private static HttpURLConnection tryRequest(String apiCall)
+      throws IOException {
+    // Configure the connection (but don't actually send the request yet)
+    URL requestURL = new URL("http://localhost:" + Spark.port() + "/" + apiCall);
+    HttpURLConnection clientConnection = (HttpURLConnection) requestURL.openConnection();
+
+    // The default method is "GET", which is what we're using here.
+    // If we were using "POST", we'd need to say so.
+    clientConnection.setRequestMethod("GET");
+
+    clientConnection.connect();
+    return clientConnection;
+  }
+  @Test
+  public void testBroadbandGoodArg() throws IOException {
+    HttpURLConnection clientConnection = tryRequest("broadband","massachusetts",
+        "middlesex");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, clientConnection.getResponseCode());
+
+    // Now we need to see whether we've got the expected Json response.
+    Moshi moshi = new Moshi.Builder().build();
+    DataSuccessResponse response =
+        moshi
+            .adapter(DataSuccessResponse.class)
+            .fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+
+    System.out.println(response);
+    // ^ If that succeeds, we got the expected response. Notice that this is *NOT* an exception, but
+    // a real Json reply.
+    assertEquals(response.responseMap().get("data").toString().trim(),
+        "[{county=Cambridge, broadband=47}]");
+    assertEquals(response.responseMap().get("county"),
+        "middlesex");
+    assertEquals(response.responseMap().get("state"),
+        "massachusetts");
+    clientConnection.disconnect();
+  }
+
+  @Test
+  public void testBroadbandNoCounty() throws IOException {
+    HttpURLConnection clientConnection = tryRequest("broadband","massachusetts",
+        "");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, clientConnection.getResponseCode());
+
+    // Now we need to see whether we've got the expected Json response.
+    Moshi moshi = new Moshi.Builder().build();
+    DataSuccessResponse response =
+        moshi
+            .adapter(DataSuccessResponse.class)
+            .fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+
+    System.out.println(response);
+    // ^ If that succeeds, we got the expected response. Notice that this is *NOT* an exception, but
+    // a real Json reply.
+    assertEquals(response.responseMap().get("data").toString().trim(),
+        "[{county=Cambridge, broadband=47}]");
+    assertEquals(response.responseMap().get("county"),
+        "");
+    assertEquals(response.responseMap().get("state"),
+        "massachusetts");
+    clientConnection.disconnect();
+  }
+  @Test
+  public void testBroadbandNoState() throws IOException {
+    HttpURLConnection clientConnection = tryRequest("broadband","",
+        "");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, clientConnection.getResponseCode());
+
+    // Now we need to see whether we've got the expected Json response.
+    Moshi moshi = new Moshi.Builder().build();
+    DataSuccessResponse response =
+        moshi
+            .adapter(DataSuccessResponse.class)
+            .fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+
+    System.out.println(response);
+    // ^ If that succeeds, we got the expected response. Notice that this is *NOT* an exception, but
+    // a real Json reply.
+    assertEquals(response.responseMap().get("data").toString().trim(),
+        "[{county=Cambridge, broadband=47}]");
+    assertEquals(response.responseMap().get("county"),
+        "");
+    assertEquals(response.responseMap().get("state"),
+        "");
+    clientConnection.disconnect();
+  }
+  @Test
+  public void testBroadbandArg2Words() throws IOException {
+    HttpURLConnection clientConnection = tryRequest("broadband","massachusetts",
+        "gag_city");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, clientConnection.getResponseCode());
+
+    // Now we need to see whether we've got the expected Json response.
+    Moshi moshi = new Moshi.Builder().build();
+    ErrorResponse response =
+        moshi
+            .adapter(ErrorResponse.class)
+            .fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+
+    System.out.println(response);
+    // ^ If that succeeds, we got the expected response. Notice that this is *NOT* an exception, but
+    // a real Json reply.
+    assertEquals("success", response.result());
+    clientConnection.disconnect();
+  }
+
+  @Test
+  public void testBadConnection() throws IOException {
+    HttpURLConnection clientConnection = tryRequest("broadband");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, clientConnection.getResponseCode());
+
+    // Now we need to see whether we've got the expected Json response.
+    Moshi moshi = new Moshi.Builder().build();
+    ErrorResponse response =
+        moshi
+            .adapter(ErrorResponse.class)
+            .fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+
+    System.out.println(response);
+    // ^ If that succeeds, we got the expected response. Notice that this is *NOT* an exception, but
+    // a real Json reply.
+    assertEquals("error", response.result());
+    clientConnection.disconnect();
   }
 
 }
